fc5f08c2c79c31946147f418f7665662d8fe67b5
prasanna212 <prasanna.arthum@gmail.com>
2016-08-08 11:42:05 +0530

    Create MetricUtil.java

---
+++ MetricUtil.java
@@ -0,0 +1,53 @@
+package books;
+
+public class MetricUtil {
+
+	public int nPara()
+	{
+		return 0;
+	}
+	
+	double getAvgParaLength()
+	{
+		return 0;
+	}
+	
+	double getAvgSentenceLength()
+	{
+		return 0;
+	}
+	
+	double getAvgWordLength()
+	{
+		return 0;
+	}
+	
+	double getPunctDensity()
+	{
+		return 0;
+	}
+	
+	int getTotalWords()
+	{
+		return 0;
+	}
+	
+	int getUniqueWordCount()
+	{
+		return 0;
+	}
+	double getVocab()
+	{
+		return 0;
+	}
+	
+	double[] getLetterFrequency()
+	{
+		return null;
+	}
+	
+	double  getWordToSentenceRatio()
+	{
+		return 0;
+	}
+}
